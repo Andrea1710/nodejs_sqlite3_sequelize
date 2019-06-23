35f5cad7231065a2a652ced546fca234cd4dc3cc
Andrea <andrea.belluccia@gmail.com>
2019-06-23 19:30:35 +0200

    Added CRUD operation for Tasks table

---
+++ src/routes/tasks.js
@@ -0,0 +1,55 @@
+module.exports = app => {
+  const Tasks = app.db.models.Tasks;
+
+  app
+    .route("/tasks")
+    .get((req, res) => {
+      Tasks.findAll({})
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
+    })
+
+    .post((req, res) => {
+      Tasks.create(req.body)
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
+    });
+
+  app
+    .route("/tasks/:id")
+    .get((req, res) => {
+      Tasks.findOne({ where: req.params })
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
+    })
+    .put((req, res) => {
+      Tasks.update(req.body, { where: req.params })
+        .then(result => {
+          res.sendStatus(204);
+        })
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
+    })
+    .delete((req, res) => {
+      Tasks.destroy({ where: req.params })
+        .then(result => {
+          res.sendStatus(204);
+        })
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
+    });
+};
